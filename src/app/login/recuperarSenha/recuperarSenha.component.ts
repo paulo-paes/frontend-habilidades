9a05300c95f9c52c468766938ae10c7382c7eb43
Paulo <phpaes99@gmail.com>
2021-11-19 12:20:15 -0300

    Criando serviço de recuperar senha

--- src/app/login/recuperarSenha/recuperarSenha.component.ts
+++ src/app/login/recuperarSenha/recuperarSenha.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
+import { EmailCadastradoValidatorService } from "../cadastro/emailCadastrado.validator.service";
+import { RecuperarSenhaService } from "./recuperarSenha.service";
 
 @Component({
     templateUrl: './recuperarSenha.component.html',
@@ -12,12 +14,25 @@ export class RecuperarSenhaComponent implements OnInit {
 
     constructor(
         private router: Router,
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private emailCadastroValidatorService: EmailCadastradoValidatorService,
+        private recuperarSenhaService: RecuperarSenhaService
     ) {}
 
-    
+
     ngOnInit(): void {
-        throw new Error("Method not implemented.");
+        this.recuperarSenhaForm = this.formBuilder.group({
+            email: ['', [Validators.email, Validators.required]]
+        })
+    }
+
+    enviarEmail(event: Event){
+        event.preventDefault();
+        const email: Object = this.recuperarSenhaForm.getRawValue();
+        console.log(email)
+        this.recuperarSenhaService
+            .enviaEmail(email)
+            .subscribe(() => console.log("success"));
     }
 
     voltar(){
---
+++ src/app/login/recuperarSenha/recuperarSenha.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+
+const API = environment.API;
+
+@Injectable({providedIn: 'root'})
+export class RecuperarSenhaService {
+
+
+    constructor(
+        private http: HttpClient
+    ) {}
+
+    enviaEmail(email: Object){
+        return this.http.post(API + 'recuperarsenha', email)
+    }
+}
